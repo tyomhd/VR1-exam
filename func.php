6a170ac09095058a88e329f4c3319294a0ec27e5
Artyom Likhachev <alikhach@klass2-21-l.intra.itcollege.ee>
2016-06-12 14:54:31 +0300

    last

--- func.php
+++ func.php
@@ -14,7 +14,7 @@ global $connection;
 if(!isset($_SESSION['LAST_ACTIVITY'])){
 		$_SESSION['LAST_ACTIVITY'] = time();
 }
-if(isset($_SESSION['LAST_ACTIVITY']) && ($_SESSION['LAST_ACTIVITY'] + 30 * 60 > time())){
+if(isset($_SESSION['LAST_ACTIVITY']) && ($_SESSION['LAST_ACTIVITY'] + 30 * 60 < time())){
 mysqli_query($connection, "UPDATE alikhach_exam SET likescount=likescount+1");
 mysqli_close($connection);
 header("Refresh:0; url=exam.php");
--- like.php
+++ like.php
@@ -6,9 +6,15 @@ global $connection;
 	$db="test";
 	$connection = mysqli_connect($host, $user, $password, $db) or die("ei saa ühendust mootoriga- ".mysqli_error());
 	mysqli_query($connection, "SET CHARACTER SET UTF8") or die("Ei saanud baasi utf-8-sse - ".mysqli_error($connection));
+if(!isset($_SESSION['LAST_ACTIVITY'])){
+		$_SESSION['LAST_ACTIVITY'] = time();
+}
+if(isset($_SESSION['LAST_ACTIVITY']) && ($_SESSION['LAST_ACTIVITY'] + 30 * 60 > time())){
 mysqli_query($connection, "UPDATE alikhach_exam SET likescount=likescount+1");
 mysqli_close($connection);
 header("Refresh:0; url=exam.php");
-
+} else{
+	header("Refresh:0; url=exam.php");
+}
 
 ?>
